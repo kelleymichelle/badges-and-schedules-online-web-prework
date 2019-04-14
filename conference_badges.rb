e516c4966e59fd9c4f445b0d0911d203d152dfdf
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-14 00:36:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -32,8 +32,9 @@ def printer(attendees)
  batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  
-  
-end
+assign_rooms(attendees).each do |room_assignments|
+  puts room_assignments
+ end
 
+end
 
