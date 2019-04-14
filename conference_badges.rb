03e8cb9e6cd4a4541f59fe6fce217cd931716b75
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-14 00:15:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  arr = [ ]
+   
+   attendees.each do |name|
+     badge = badge_maker(name)
+      arr << badge
+   end  
+  return arr
+end  
+
+def assign_rooms(attendees)
+  room_assignments = [ ]
+    room = 1
+
+  attendees.each do |attendee|
+    
+     room_assignments << "Hello, #{attendee}! You'll be assigned to room #{room}!"
+        room += 1
+  end
+  
+  return room_assignments
+  
+end  
+
+def printer
+  puts batch_badge_creator
+  puts assign_rooms
+end
+
+
